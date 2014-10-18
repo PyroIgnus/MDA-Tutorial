3225da4369d7a3e6707d9eb01fdca61cdba2da46
ziggyzsolt <k.zaghloul@mail.utoronto.ca>
2014-10-18 18:46:56 -0400

    FirstClassKareem - First Commit
    Created class

---
+++ FirstClassKareem.cpp
@@ -0,0 +1,14 @@
+#include(iostream);
+#include(Math.h);
+
+Class FirstClassKareem{
+	int firstNumber, secondNumber;
+	public double TestFunction(int parameter1, int parameter2)
+} Test1
+
+
+double FirstClass::TestFunction(int AddValue, int MultipValue) {
+	return ((6)*(MultipValue-AddValue)/(10))
+}
+
+
